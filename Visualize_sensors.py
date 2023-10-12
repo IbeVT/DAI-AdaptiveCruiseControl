0f7041924a718802b36c531fb220e658aba2b205
MeurisLukas <lukas-meuris@hotmail.be>
2023-10-12 16:58:23 +0200

    added possible radar visualization.

--- Visualize_sensors.py
+++ Visualize_sensors.py
@@ -22,6 +22,12 @@ import carla
 import argparse
 import random
 import time
+import datetime
+import logging
+import math
+import random
+import re
+import weakref
 import numpy as np
 
 try:
@@ -88,6 +94,7 @@ class DisplayManager:
 
 class SensorManager:
     def __init__(self, world, display_man, sensor_type, transform, attached, sensor_options, display_pos):
+        self.velocity_range = 7.5
         self.surface = None
         self.world = world
         self.display_man = display_man
@@ -239,6 +246,37 @@ class SensorManager:
         points = np.frombuffer(radar_data.raw_data, dtype=np.dtype('f4'))
         points = np.reshape(points, (len(radar_data), 4))
         print(points)
+
+        current_rot = radar_data.transform.rotation
+        for detect in radar_data:
+            azi = math.degrees(detect.azimuth)
+            alt = math.degrees(detect.altitude)
+            # The 0.25 adjusts a bit the distance so the dots can
+            # be properly seen
+            fw_vec = carla.Vector3D(x=detect.depth - 0.25)
+            carla.Transform(
+                carla.Location(),
+                carla.Rotation(
+                    pitch=current_rot.pitch + alt,
+                    yaw=current_rot.yaw + azi,
+                    roll=current_rot.roll)).transform(fw_vec)
+
+            def clamp(min_v, max_v, value):
+                return max(min_v, min(value, max_v))
+
+            norm_velocity = detect.velocity / self.velocity_range  # range [-1, 1]
+            r = int(clamp(0.0, 1.0, 1.0 - norm_velocity) * 255.0)
+            g = int(clamp(0.0, 1.0, 1.0 - abs(norm_velocity)) * 255.0)
+            b = int(abs(clamp(- 1.0, 0.0, - 1.0 - norm_velocity)) * 255.0)
+
+            if self.display_man.render_enabled():
+                self.surface = pygame.surfarray.draw_point(
+                    radar_data.transform.location + fw_vec,
+                    size=0.075,
+                    life_time=0.06,
+                    persistent_lines=False,
+                    color=carla.Color(r, g, b)
+                )
 
         t_end = self.timer.time()
         self.time_processing += (t_end - t_start)
