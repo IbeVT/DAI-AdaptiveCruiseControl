cb85c63b42d2d9c07802b6e21daf2fe002fd3c40
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-10-17 15:27:41 +0200

    Add training script for YOLO based on Carla-Object-Detection-Dataset

---
+++ train_yolo.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Start from pretrained model
+model = YOLO('yolov8n.pt')
+
+# Train the model on the custom dataset
+results = model.train(data="./yolo_train.yaml")
+print("results:\n" + results)
+
+model.export()
+
+# Validate the model on the test dataset
+metrics = model.val()
+print("Validation metric\n" + metrics)
