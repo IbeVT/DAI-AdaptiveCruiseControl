73b9cc6481fcb3d6853cde38febab89f79c55a61
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-10-19 13:57:10 +0200

    Switch logger to CometML

--- train_yolo.py
+++ train_yolo.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
+import comet_ml
 
+comet_ml.init()
 # Start from pretrained model
 model = YOLO('yolov8n.pt')
 
 # Train the model on the custom dataset
 results = model.train(data="./yolo_train.yaml", workers=1)
-print("results:\n" + results)
 
 model.export()
 
 # Validate the model on the test dataset
 metrics = model.val()
-print("Validation metric\n" + metrics)
