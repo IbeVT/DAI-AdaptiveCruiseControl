27b5fbcbf0b622e54f63cad680103fb826c93d14
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-11-16 15:00:30 +0100

    Debug

--- ComputerVision.py
+++ ComputerVision.py
@@ -69,11 +69,9 @@ class ComputerVision:
             print('Median depth:', round(np.median(object_point_depths), 1), '  Median speed:',
                   round(np.median(object_point_speeds), 1))
 
-            print(image.shape)
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
             # Add bounding box to image
             print(f"x_lower: {x_lower}, y_lower: {y_lower}, x_upper: {x_upper}, y_upper: {y_upper}")
-            # cv2.rectangle(image, (x_lower, y_lower), (x_upper, y_upper), (0, 255, 0), 2)
+            cv2.rectangle(image, (x_lower, y_lower), (x_upper, y_upper), (0, 255, 0), 2)
             # Add statistics to image
             text = f'Depth: ({np.median(object_point_depths)}, speed: {np.median(object_point_speeds)})'
             font = cv2.FONT_HERSHEY_SIMPLEX
