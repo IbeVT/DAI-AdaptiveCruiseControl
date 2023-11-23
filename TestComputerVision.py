05b4a0059d15f539aa25a7a1a513e9b1bb6c0f91
Ibe Van Thillo <Ibe.VanThillo@student.uantwerpen.be>
2023-11-23 17:58:24 +0100

    Change sensor_tick

--- TestComputerVision.py
+++ TestComputerVision.py
@@ -370,7 +370,7 @@ def run_simulation(args, client):
                       carla.Transform(carla.Location(x=0, z=2.4)),
                       vehicle,
                       {'horizontal_fov': '90', 'points_per_second': '5000', 'range': '100',
-                       'sensor_tick': '0.5', 'vertical_fov': '60'}, display_pos=[0, 0], computer_vision=computer_vision)
+                       'sensor_tick': '0', 'vertical_fov': '60'}, display_pos=[0, 0], computer_vision=computer_vision)
 
         # But the city now is probably quite empty, let's add a few more vehicles.
         transform.location += carla.Location(x=40, y=-3.2)
