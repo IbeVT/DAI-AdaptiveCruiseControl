55de841c2c102fa5f2eb9c1dae20784e5c6cf437
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-11-28 14:50:09 +0100

    solve environment.environment path attempt 7

--- carla_RL/train_RL.py
+++ carla_RL/train_RL.py
@@ -9,7 +9,7 @@ import gym
 from setuptools import setup
 
 from gym.envs.registration import register
-import rllib_integration_new.environment.environment.gym_carla
+import carla_RL.environment.environment.gym_carla
 
 import os
 print(os.getcwd())
