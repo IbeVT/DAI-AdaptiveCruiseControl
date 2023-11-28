823507729e2deb6d38738fb7c4125d6ca3c52240
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-11-28 16:09:59 +0100

    actor dying solved attempt 10

--- carla_RL/environment/environment/setup.py
+++ carla_RL/environment/environment/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='gym_carla',
       version='0.0.1',
-      install_requires=['gym', 'pygame'] 
+      install_requires=['gym', 'pygame'],
+      packages=find_packages()
 )
