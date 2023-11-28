4abfd1afe5293a684f1ccb104365143d5e1c34e1
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-11-28 15:27:35 +0100

    actor dying solved attempt 3

--- carla_RL/train_RL.py
+++ carla_RL/train_RL.py
@@ -14,12 +14,10 @@ sys.path.append('carla_RL/environment/environment')
 sys.path.append('carla_RL/environment/environment/gym_carla')
 import gym_carla
 
-import os
-print(os.getcwd())
-register(
+'''register(
     id='CustomCarlaEnv',
     entry_point='gym_carla.envs:CarlaEnv',
-)
+)'''
 
 """setup(name='gym_carla',
       version='0.0.1',
