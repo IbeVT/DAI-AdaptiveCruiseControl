012101f574d9d9eb2fb832ad7a3dd5ae7d4c5bfa
MeurisLukas <lukas-meuris@hotmail.be>
2023-11-30 15:03:36 +0100

    finished CarlaInterface.py

--- CarlaInterface.py
+++ CarlaInterface.py
@@ -1,11 +1,13 @@
 """
-This script will spawn a car with camera and radar sensors.
-The car will drive automatically through the town
-and collect camera and radar data every second and save it to the disk.
+Carla interface testing file
+**
+..
+**
 """
 
 import glob
 import os
+import re
 import sys
 
 try:
@@ -32,6 +34,16 @@ except ImportError:
     raise RuntimeError('cannot import pygame, make sure pygame package is installed')
 
 
+def find_weather_presets():
+    """Method to find weather presets"""
+    rgx = re.compile('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)')
+
+    def name(x): return ' '.join(m.group(0) for m in rgx.finditer(x))
+
+    presets = [x for x in dir(carla.WeatherParameters) if re.match('[A-Z].+', x)]
+    return [(getattr(carla.WeatherParameters, x), name(x)) for x in presets]
+
+
 class CustomTimer:
     def __init__(self):
         try:
@@ -41,6 +53,7 @@ class CustomTimer:
 
     def time(self):
         return self.timer()
+
 
 # The DisplayManager is in charge of displaying the data collected from the sensors on the screen.
 class DisplayManager:
@@ -83,7 +96,34 @@ class DisplayManager:
             s.destroy()
 
     def render_enabled(self):
-        return self.display != None
+        return self.display is not None
+
+
+def save_rbg_image_to_disk(image):
+    image.save_to_disk(f"RGBCameraData/{image.frame}.png")
+
+
+def save_radar_data_to_disk(data_points):
+    if not os.path.isfile('./RadarData.csv'):
+        print(f" RadarData.csv created. ")
+        # We create a csv file to save our radar-data.
+        # Let's define the headings of our csv file and save them.
+        header = ['Velocity', 'Altitude', 'Azimuth', 'Depth']
+        with open('RadarData.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+
+    # sava radar data to disk
+    data_points[:, 1] = np.degrees(data_points[:, 1])  # change Altitude from rad to degrees.
+    data_points[:, 2] = np.degrees(data_points[:, 2])  # change Azimuth from rad to degrees.
+    data_points[:, 0] = np.round(data_points[:, 0], 0)  # round Velocity.
+    data_points[:, 3] = np.round(data_points[:, 3], 0)  # round Depth.
+    try:
+        with open('RadarData.csv', 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(data_points)
+    except Exception as e:
+        print(f"Error writing row to CSV: {e}")
 
 
 class SensorManager:
@@ -111,7 +151,7 @@ class SensorManager:
                 camera_bp.set_attribute(key, sensor_options[key])
 
             camera = self.world.spawn_actor(camera_bp, transform, attach_to=attached)
-            camera.listen(self.save_rgb_image)
+            camera.listen(self.display_rgb_image)
             return camera
 
         elif sensor_type == "Radar":
@@ -120,7 +160,7 @@ class SensorManager:
                 radar_bp.set_attribute(key, sensor_options[key])
 
             radar = self.world.spawn_actor(radar_bp, transform, attach_to=attached)
-            radar.listen(self.save_radar_image)
+            radar.listen(self.display_radar_image)
             return radar
 
         else:
@@ -129,7 +169,7 @@ class SensorManager:
     def get_sensor(self):
         return self.sensor
 
-    def save_rgb_image(self, image):
+    def display_rgb_image(self, image):
         t_start = self.timer.time()
 
         image.convert(carla.ColorConverter.Raw)
@@ -143,29 +183,20 @@ class SensorManager:
             self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
 
         # save camera data to disk.
-        image.save_to_disk(f"RGBCameraData/{image.frame}.png")
+        # save_rbg_image_to_disk(image)
 
         t_end = self.timer.time()
         self.time_processing += (t_end - t_start)
         self.tics_processing += 1
 
-    def save_radar_image(self, radar_data):
+    def display_radar_image(self, radar_data):
         t_start = self.timer.time()
         points = np.frombuffer(radar_data.raw_data, dtype=np.dtype('f4'))
         points = np.reshape(points, (len(radar_data), 4))
         data_points = points.copy()
 
-        # sava radar data to disk
-        data_points[:, 1] = np.degrees(data_points[:, 1])  # change Altitude from rad to degrees.
-        data_points[:, 2] = np.degrees(data_points[:, 2])  # change Azimuth from rad to degrees.
-        data_points[:, 0] = np.round(data_points[:, 0], 0)  # round Velocity.
-        data_points[:, 3] = np.round(data_points[:, 3], 0)  # round Depth.
-        try:
-            with open('RadarData.csv', 'a') as file:
-                writer = csv.writer(file)
-                writer.writerow(data_points)
-        except Exception as e:
-            print(f"Error writing row to CSV: {e}")
+        # save radar data to disk.
+        # save_radar_data_to_disk(data_points)
 
         current_rot = radar_data.transform.rotation
         for detect in radar_data:
@@ -184,6 +215,7 @@ class SensorManager:
             # give color to radar data: white = neutral; red= move closer; blue=moving away.
             def clamp(min_v, max_v, value):
                 return max(min_v, min(value, max_v))
+
             velocity_range = 7.5  # m/s
             norm_velocity = detect.velocity / velocity_range  # range [-1, 1]
             r = int(clamp(0.0, 1.0, 1.0 - norm_velocity) * 255.0)
@@ -199,7 +231,6 @@ class SensorManager:
                     color=carla.Color(r, g, b)
                 )
 
-
         t_end = self.timer.time()
         self.time_processing += (t_end - t_start)
         self.tics_processing += 1
@@ -213,38 +244,87 @@ class SensorManager:
         self.sensor.destroy()
 
 
-def run_simulation(args, client):
-    """This function performed one test run using the args parameters
-    and connecting to the carla client passed.
-    """
+def set_town(client):
+    # set a random map to spawn our actors in.
+    # client.load_world(None)
+    maps = ['Town01', 'Town02', 'Town03', 'Town04', 'Town05', 'Town06', 'Town07', 'Town10', 'Town11', 'Town12']
+    new_map = maps[random.randint(0, 10)]
+    print('Map: %s' % new_map)
+    client.load_world(new_map)
 
+
+def set_weather(world):
+    # set a random weather condition.
+    weather = random.choice(find_weather_presets())
+    print('Weather: %s' % weather[1])
+    world.set_weather(weather[0])
+
+
+def manual_control(ego_vehicle, throttle, brake):
+    control = carla.VehicleControl()
+
+    # Manual throttle and brake
+    control.throttle = throttle
+    control.brake = brake
+
+    return control
+
+
+def game_loop(args):
+    world = None
+    original_settings = None
     display_manager = None
-    vehicle = None
-    vehicle_list = []
     timer = CustomTimer()
+    actor_list = []
+    vehicle_list = []
 
     try:
+        client = carla.Client(args.host, args.port)
+        client.set_timeout(5.0)
+
+        # set a random town from our list.
+        set_town(client)
+
         # Getting the world
         world = client.get_world()
+        blueprint = world.get_blueprint_library()
+        spawnPoints = world.get_map().get_spawn_points()
+
         # get original_settings to reset the world after use.
         original_settings = world.get_settings()
 
         if args.sync:
-            traffic_manager = client.get_trafficmanager(8000)
+            traffic_manager = client.get_trafficmanager(args.tm_port)
+            if args.respawn:
+                traffic_manager.set_respawn_dormant_vehicles(True)
+            if args.hybrid:
+                traffic_manager.set_hybrid_physics_mode(True)
+                traffic_manager.set_hybrid_physics_radius(70.0)
+            if args.seed is not None:
+                traffic_manager.set_random_device_seed(args.seed)
             settings = world.get_settings()
             traffic_manager.set_synchronous_mode(True)
             settings.synchronous_mode = True
             settings.fixed_delta_seconds = 0.05
             world.apply_settings(settings)
 
+        # set a random weather preset.
+        set_weather(world)
+
         # Instantiating the vehicle to which we attached the sensors
-        transform = random.choice(world.get_map().get_spawn_points())
-        ego_bp = random.choice(world.get_blueprint_library().filter('vehicle'))
+        transform = random.choice(spawnPoints)
+        ego_bp = random.choice(blueprint.filter('vehicle'))
         ego_bp.set_attribute('role_name', 'ego')
-        vehicle = world.spawn_actor(ego_bp, transform)
-        vehicle_list.append(vehicle)
-        print('created %s' % vehicle.type_id)
-        vehicle.set_autopilot(True)
+        ego_vehicle = world.spawn_actor(ego_bp, transform)
+        vehicle_list.append(ego_vehicle)
+        print('created ego: %s' % ego_vehicle.type_id)
+        ego_vehicle.set_autopilot(True)
+
+        # set the spectator to follow our ego_vehicle.
+        spectator = world.get_spectator()
+        transform = carla.Transform(ego_vehicle.get_transform().transform(carla.Location(x=-4, z=2.5)),
+                                    ego_vehicle.get_transform().rotation)
+        spectator.set_transform(transform)
 
         # Display Manager organize all the sensors and its display in a window
         # It can easily configure the grid and the total window size
@@ -253,22 +333,22 @@ def run_simulation(args, client):
         # Then, SensorManager is used to spawn RGBCamera and Radar and assign each of them to a grid position.
         SensorManager(world, display_manager, 'RGBCamera',
                       carla.Transform(carla.Location(x=0, z=2.4), carla.Rotation(yaw=+00)),
-                      vehicle, {'sensor_tick': '1.0'}, display_pos=[0, 0])
+                      ego_vehicle, {'sensor_tick': '0.0'}, display_pos=[0, 0])
 
         SensorManager(world, display_manager, 'Radar',
                       carla.Transform(carla.Location(x=0, z=2.4)),
-                      vehicle,
+                      ego_vehicle,
                       {'horizontal_fov': '90', 'points_per_second': '1500', 'range': '75',
-                       'sensor_tick': '1.0', 'vertical_fov': '60'}, display_pos=[0, 0])
+                       'sensor_tick': '0.0', 'vertical_fov': '60'}, display_pos=[0, 0])
 
         # But the city now is probably quite empty, let's add a few more vehicles.
         transform.location += carla.Location(x=40, y=-3.2)
         transform.rotation.yaw = -180.0
         number_of_vehicles = 0
-        while number_of_vehicles < 10:
+        while number_of_vehicles < args.number_of_vehicles:
             transform.location.x += 8.0
 
-            bp = random.choice(world.get_blueprint_library().filter('vehicle'))
+            bp = random.choice(blueprint.filter('vehicle'))
 
             # This time we are using try_spawn_actor. If the spot is already
             # occupied by another object, the function will return None.
@@ -279,17 +359,28 @@ def run_simulation(args, client):
                 number_of_vehicles += 1
                 print('created %s' % npc.type_id)
 
-        # We create a csv file to save our radar-data.
-        # Let's define the headings of our csv file and save them.
-        header = ['Velocity', 'Altitude', 'Azimuth', 'Depth']
-        with open('RadarData.csv', 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(header)
+        # Set automatic vehicle lights update if specified
+        if args.car_lights_on:
+            all_vehicle_actors = world.get_actors(vehicle_list)
+            for actor in all_vehicle_actors:
+                traffic_manager.update_vehicle_lights(actor, True)
 
         # Simulation loop
         call_exit = False
-        time_init_sim = timer.time()
         while True:
+            # controlling throttle and brake
+            """ RL output: 
+            throttle_input = 0
+            brake_input = 0
+            control = manual_control(ego_vehicle, throttle=throttle_input, brake=brake_input)
+            ego_vehicle.apply_control(control)
+            """
+
+            # Update the spectator's position to follow the ego vehicle
+            transform = carla.Transform(ego_vehicle.get_transform().transform(carla.Location(x=-4, z=2.5)),
+                                        ego_vehicle.get_transform().rotation)
+            spectator.set_transform(transform)
+
             # Carla Tick
             if args.sync:
                 world.tick()
@@ -315,13 +406,17 @@ def run_simulation(args, client):
             display_manager.destroy()
 
         client.apply_batch([carla.command.DestroyActor(x) for x in vehicle_list])
+        client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
+
+        if traffic_manager:
+            traffic_manager.set_synchronous_mode(False)
 
         world.apply_settings(original_settings)
 
 
 def main():
     argparser = argparse.ArgumentParser(
-        description='CARLA Sensors')
+        description='CARLA World generation')
     argparser.add_argument(
         '--host',
         metavar='H',
@@ -333,6 +428,73 @@ def main():
         default=2000,
         type=int,
         help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '-n', '--number-of-vehicles',
+        metavar='N',
+        default=random.randint(20, 150),
+        type=int,
+        help='Number of vehicles (between 6 and 150)')
+    argparser.add_argument(
+        '-w', '--number-of-walkers',
+        metavar='W',
+        default=random.randint(2, 50),
+        type=int,
+        help='Number of walkers (between 2 and 50)')
+    argparser.add_argument(
+        '--safe',
+        action='store_true',
+        help='Avoid spawning vehicles prone to accidents')
+    argparser.add_argument(
+        '--filterv',
+        metavar='PATTERN',
+        default='vehicle.*',
+        help='Filter vehicle model (default: "vehicle.*")')
+    argparser.add_argument(
+        '--generationv',
+        metavar='G',
+        default='All',
+        help='restrict to certain vehicle generation (values: "1","2","All" - default: "All")')
+    argparser.add_argument(
+        '--filterw',
+        metavar='PATTERN',
+        default='walker.pedestrian.*',
+        help='Filter pedestrian type (default: "walker.pedestrian.*")')
+    argparser.add_argument(
+        '--generationw',
+        metavar='G',
+        default='2',
+        help='restrict to certain pedestrian generation (values: "1","2","All" - default: "2")')
+    argparser.add_argument(
+        '--tm-port',
+        metavar='P',
+        default=8000,
+        type=int,
+        help='Port to communicate with TM (default: 8000)')
+    argparser.add_argument(
+        '--hybrid',
+        action='store_true',
+        help='Activate hybrid mode for Traffic Manager')
+    argparser.add_argument(
+        '-s', '--seed',
+        metavar='S',
+        type=int,
+        help='Set random device seed and deterministic mode for Traffic Manager')
+    argparser.add_argument(
+        '--seedw',
+        metavar='S',
+        default=0,
+        type=int,
+        help='Set the seed for pedestrians module')
+    argparser.add_argument(
+        '--car-lights-on',
+        action='store_true',
+        default=False,
+        help='Enable automatic car light management')
+    argparser.add_argument(
+        '--respawn',
+        action='store_true',
+        default=False,
+        help='Automatically respawn dormant vehicles (only in large maps)')
     argparser.add_argument(
         '--sync',
         action='store_true',
@@ -354,10 +516,7 @@ def main():
     args.width, args.height = [int(x) for x in args.res.split('x')]
 
     try:
-        client = carla.Client(args.host, args.port)
-        client.set_timeout(5.0)
-
-        run_simulation(args, client)
+        game_loop(args)
 
     except KeyboardInterrupt:
         print('\nCancelled by user. Bye!')
