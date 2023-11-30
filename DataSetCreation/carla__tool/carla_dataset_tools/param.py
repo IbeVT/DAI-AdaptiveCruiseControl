fb9767c938b4311608eb7963b5c4c38523265fb1
Albertokeroro <alb.moro.2000@gmail.com>
2023-11-30 16:15:04 +0000

    fixed pathing

--- DataSetCreation/carla__tool/carla_dataset_tools/param.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from pathlib import Path
-
-# Project Root Path
-ROOT_PATH = Path(__file__).parent.as_posix()
-print("Project Root PATH: {}".format(ROOT_PATH))
-
-RAW_DATA_PATH = "{}/{}".format(ROOT_PATH, 'raw_data')
-DATASET_PATH = "{}/{}".format(ROOT_PATH, 'dataset')
