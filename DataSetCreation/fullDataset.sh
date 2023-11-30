8defcdb7efa91bbec26e3e2c79df2cfde085e942
Albertokeroro <alb.moro.2000@gmail.com>
2023-11-30 15:51:32 +0000

    automatic dataset creating script

---
+++ DataSetCreation/fullDataset.sh
@@ -0,0 +1,14 @@
+script_pida=$(python3 ./carla__tool/carla_dataset_
+tools/data_record/data_recorder_a.py &)
+
+sleep 10
+
+kill -INT $script_pida
+
+script_pidb=$(python3 ./carla__tool/carla_dataset_
+tools/data_record/data_recorder_b.py &)
+
+
+sleep  10
+
+kill -INT $script_pidb
