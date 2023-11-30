1c3217c85e5d809b7a676a2ac873cbdb3496f98d
Albertokeroro <alb.moro.2000@gmail.com>
2023-11-30 16:57:17 +0000

    script for 10 differents combinations between weather and towns, can be upgraded later

--- DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
+++ DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
@@ -1,14 +1,52 @@
 #!/bin/sh
 
-script_pida=$(python3 data_recorder_a.py &)
-
+script_pid=$(python3 data_recorder_a.py -w worlds/world1.json &)
+sleep 20
+kill -INT $script_pid
 sleep 10
 
-kill -INT $script_pida
+script_pid=$(python3 data_recorder_b.py -w worlds/world2.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
 
-script_pidb=$(python3 data_recorder_b.py &)
+script_pid=$(python3 data_recorder_c.py -w worlds/world3.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
 
+script_pid=$(python3 data_recorder_d.py -w worlds/world4.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
 
-sleep  10
+script_pid=$(python3 data_recorder_e.py -w worlds/world5.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
 
-kill -INT $script_pidb
+script_pid=$(python3 data_recorder_e.py -w worlds/world1.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
+
+script_pid=$(python3 data_recorder_d.py -w worlds/world2.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
+
+script_pid=$(python3 data_recorder_b.py -w worlds/world3.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
+
+script_pid=$(python3 data_recorder_b.py -w worlds/world4.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
+
+script_pid=$(python3 data_recorder_a.py -w worlds/world5.json &)
+sleep  20
+kill -INT $script_pid
+sleep 10
+
