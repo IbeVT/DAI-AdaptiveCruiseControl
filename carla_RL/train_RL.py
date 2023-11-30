bfbbed0e7865e52db247333e8679f58c9830bd5d
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-11-30 15:36:26 +0100

    Carla crashing due to load_world attempt 14

--- carla_RL/train_RL.py
+++ carla_RL/train_RL.py
@@ -81,7 +81,8 @@ if __name__ == "__main__":
             "lr": 5e-3   #tune.grid_search([5e-3, 5e-4])
         },
         run_config=train.RunConfig(
-            disable_env_checking=True,
+            ignore_worker_failures=True,
+            #disable_env_checking=True,
             #stop={"episode_reward_mean": 30},
             callbacks=[
                 WandbLoggerCallback(
