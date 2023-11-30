35d6a997ab697717fd08b193d82ebcffb303c107
Albertokeroro <alb.moro.2000@gmail.com>
2023-11-30 15:59:13 +0000

    path fix

--- DataSetCreation/fullDataset.sh
+++ DataSetCreation/fullDataset.sh
@@ -1,12 +1,15 @@
-script_pida=$(python3 ./carla__tool/carla_dataset_
-tools/data_record/data_recorder_a.py &)
+script_pida=$(python3 carla__tool/carla_dataset_tools/data_record/data_recorder_a.py&)
+
+
+
+
+
 
 sleep 10
 
 kill -INT $script_pida
 
-script_pidb=$(python3 ./carla__tool/carla_dataset_
-tools/data_record/data_recorder_b.py &)
+script_pidb=$(python3 carla__tool/carla_dataset_tools/data_record/data_recorder_b.py &)
 
 
 sleep  10
