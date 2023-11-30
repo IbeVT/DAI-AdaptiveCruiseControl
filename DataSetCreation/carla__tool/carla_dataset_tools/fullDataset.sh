c0684de53689ec4283aadd486b825fe3bc2610db
Albertokeroro <alb.moro.2000@gmail.com>
2023-11-30 16:40:02 +0000

    working

--- DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
+++ DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-script_pida=$(python3 carla__tool/carla_dataset_tools/data_record/data_recorder_a.py&)
+script_pida=$(python3 data_recorder_a.py &)
 
 sleep 10
 
 kill -INT $script_pida
 
-script_pidb=$(python3 carla__tool/carla_dataset_tools/data_record/data_recorder_b.py &)
+script_pidb=$(python3 data_recorder_b.py &)
 
 
 sleep  10
