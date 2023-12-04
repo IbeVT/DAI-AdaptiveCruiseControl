96f45eed8014ac6ceb88b4cdfd82a4b24def8507
Albertokeroro <alb.moro.2000@gmail.com>
2023-12-04 19:04:25 +0000

    cml info added

--- DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
+++ DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-
+echo -n "Running first simulation"
 script_pid=$(python3 data_recorder_a.py -w worlds/world1.json &)
 sleep 20
+echo -n "killing rfirst simulation"
 kill -INT $script_pid
 sleep 10
+echo -n " running second simulation"
 
 script_pid=$(python3 data_recorder_b.py -w worlds/world2.json &)
 sleep  20
 kill -INT $script_pid
 sleep 10
-
+echo -n " running third  simulation"
 script_pid=$(python3 data_recorder_c.py -w worlds/world3.json &)
 sleep  20
 kill -INT $script_pid
