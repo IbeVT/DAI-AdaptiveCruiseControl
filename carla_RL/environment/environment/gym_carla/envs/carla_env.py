2bdcd40b3545571a332a8b31f293e590cc4339f0
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-05 15:02:11 +0100

    RL debug 19

--- carla_RL/environment/environment/gym_carla/envs/carla_env.py
+++ carla_RL/environment/environment/gym_carla/envs/carla_env.py
@@ -286,7 +286,7 @@ class CarlaEnv(gym.Env):
 
     def step(self, action):
         #print('------------------------------------STEP--------------------------------------\n\n\n')
-        return (self._get_obs(), 0, False, {'waypoints': 0, 'vehicle_front': 0})
+        #return (self._get_obs(), 0, False, {'waypoints': 0, 'vehicle_front': 0})
         # Calculate acceleration and steering
         if self.discrete:
             acc = self.discrete_act[0][action // self.n_steer]
