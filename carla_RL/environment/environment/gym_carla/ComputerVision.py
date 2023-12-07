b464e5d840abb478f75371f22be3156030ac0a17
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-07 17:04:10 +0100

    Combining Computer Vision with RL 8

--- carla_RL/environment/environment/gym_carla/ComputerVision.py
+++ carla_RL/environment/environment/gym_carla/ComputerVision.py
@@ -15,7 +15,7 @@ class ComputerVision:
         self.vehicle = vehicle
         self.inverse_camera_matrix = None
         self.radar_points = None
-        self.model = YOLO('best.pt')
+        self.model = YOLO('gym_carla/best.pt')
         self.vehicle_classes = ['bus', 'bike', 'car', 'motorcycle', 'vehicle']
         self.camera_x_pixels = 720
         self.camera_y_pixels = 1280
