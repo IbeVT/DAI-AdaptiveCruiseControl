9e66c7ba0f8c338beef257235bd2b238cb13cdd3
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-07 17:40:07 +0100

    Combining Computer Vision with RL 20

--- carla_RL/environment/environment/gym_carla/envs/carla_env.py
+++ carla_RL/environment/environment/gym_carla/envs/carla_env.py
@@ -515,11 +515,18 @@ class CarlaEnv(gym.Env):
 
         # Render received data
         self.display_manager.render()
-
+        """
         obs = {
             'speed': np.array(self.ego.get_velocity().length(), dtype=np.float32),
             'distance': np.array(self.computer_vision.get_distance(), dtype=np.float32),
             'delta_V': np.array(self.computer_vision.get_delta_v(), dtype=np.float32),
+            'speed_limit': np.array(50.0, dtype=np.float32),
+            'is_red_light': np.array(1 if self.computer_vision.get_red_light() else 0, dtype=int)
+        }"""
+        obs = {
+            'speed': np.array(self.ego.get_velocity().length(), dtype=np.float32),
+            'distance': np.array(10, dtype=np.float32),
+            'delta_V': np.array(10, dtype=np.float32),
             'speed_limit': np.array(50.0, dtype=np.float32),
             'is_red_light': np.array(1 if self.computer_vision.get_red_light() else 0, dtype=int)
         }
