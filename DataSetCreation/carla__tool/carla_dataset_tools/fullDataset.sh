6846649030a4d81f231f6a2144966401b77e4427
Albertokeroro <alb.moro.2000@gmail.com>
2023-12-12 14:13:25 +0000

    removed less useful scenarios

--- DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
+++ DataSetCreation/carla__tool/carla_dataset_tools/fullDataset.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo -n "Running first simulation"
-script_pid=$(python3 data_recorder_c.py -w worlds/world2.json &)
+script_pid=$(python3 data_recorder_d.py -w worlds/world2.json &)
 sleep 20
 echo -n "killing first simulation"
 kill -INT $script_pid
 sleep 10
 echo -n " running second simulation"
 
-script_pid=$(python3 data_recorder_e.py -w worlds/world3.json &)
+script_pid=$(python3 data_recorder_b.py -w worlds/world3.json &)
 sleep  20
 kill -INT $script_pid
 sleep 10
 echo -n " running third  simulation"
-script_pid=$(python3 data_recorder_b.py -w worlds/world4.json &)
+script_pid=$(python3 data_recorder_e.py -w worlds/world4.json &)
 sleep  20
 kill -INT $script_pid
 sleep 10
@@ -24,7 +24,7 @@ kill -INT $script_pid
 sleep 10
 
 echo -n " running 5  simulation"
-script_pid=$(python3 data_recorder_e.py -w worlds/world5.json &)
+script_pid=$(python3 data_recorder_b.py -w worlds/world5.json &)
 sleep  20
 kill -INT $script_pid
 sleep 10
