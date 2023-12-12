b50178060b7796ecb284bb315c582a4342942e4b
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-12 16:19:33 +0100

    Merging computer vision with RL 30

--- carla_RL/environment/environment/gym_carla/envs/carla_env.py
+++ carla_RL/environment/environment/gym_carla/envs/carla_env.py
@@ -185,9 +185,7 @@ class CarlaEnv(gym.Env):
 
         # Clear sensor objects
         self.collision_sensor = None
-        self.camera_manager.destroy()
         self.camera_manager = None
-        self.radar_manager.destroy()
         self.radar_manager = None
         self.display_manager.destroy()
         self.display_manager = None
