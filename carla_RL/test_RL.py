b217384bbec1d4e1da1a994ce2a4b2557f797d6d
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-12 14:48:25 +0100

    Merging computer vision with RL 18

--- carla_RL/test_RL.py
+++ carla_RL/test_RL.py
@@ -1,15 +1,30 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2019: Jianyu Chen (jianyuchen@berkeley.edu).
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
+import ray
+from ray import train, tune, air
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.rllib.models import ModelCatalog
+from ray.tune.search import grid_search
 import gymnasium as gym
+
+from setuptools import setup
+
+from gymnasium.envs.registration import register
+from ray.tune.registry import register_env
+import sys
+sys.path.append('carla_RL/environment/environment')
+sys.path.append('carla_RL/environment/environment/gym_carla')
 import gym_carla
+from gym_carla.envs.carla_env import CarlaEnv
+from gymnasium.wrappers import EnvCompatibility
+import wandb
+
 import carla
-from ray import tune, air
 import ray.rllib.algorithms.ppo as ppo
+
+def env_creator(env_config=None):
+    print('-----------------------ENV_CREATOR-------------------------\n\n\n')
+    return EnvCompatibility(CarlaEnv())
+
+register_env("CustomCarlaEnv", env_creator)
 
 def main():
   # load and restore model
