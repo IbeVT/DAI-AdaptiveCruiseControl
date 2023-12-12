3b190947d3bf49a4620d48f586b388d1cbf3ca64
María Martí Valero <91225644+mariaemeuve@users.noreply.github.com>
2023-12-12 12:43:03 +0100

    update test

--- TestComputerVision_signs.py
+++ TestComputerVision_signs.py
@@ -311,7 +311,8 @@ def run_simulation(args, client):
             world.apply_settings(settings)
 
         # Instantiating the vehicle to which we attached the sensors
-        transform = random.choice(world.get_map().get_spawn_points())
+        spawn_points = world.get_map().get_spawn_points()
+        transform = random.choice(spawn_points)
         bp_lib = world.get_blueprint_library()
         ego_bp = random.choice(bp_lib.filter('vehicle'))
         ego_bp.set_attribute('role_name', 'ego')
