29715a76616d0966b0d153dc0c13a6406700d8d3
María Martí Valero <91225644+mariaemeuve@users.noreply.github.com>
2023-12-14 15:25:28 +0100

    cv update

--- ComputerVision_signs.py
+++ ComputerVision_signs.py
@@ -94,7 +94,7 @@ class ComputerVision:
             class_id = result.names[box1.cls[0].item()]
             cords = box1.xyxy[0].tolist()
             cords = [round(x) for x in cords]
-            conf = round(box.conf[0].item(), 2)
+            conf = round(box1.conf[0].item(), 2)
             print("Object type class1:", class_id)
             print("Coordinates:", cords)
             print("Probability:", conf)
