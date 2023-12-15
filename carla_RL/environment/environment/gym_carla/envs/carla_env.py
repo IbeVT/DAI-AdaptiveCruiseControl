40555b950a56e5d0facf70d357297f7e14341b6b
Stijn Van Raemdonck <van.raemdonck.stijn2@gmail.com>
2023-12-15 21:21:51 +0100

    debug not learning 3

--- carla_RL/environment/environment/gym_carla/envs/carla_env.py
+++ carla_RL/environment/environment/gym_carla/envs/carla_env.py
@@ -385,7 +385,8 @@ class CarlaEnv(gym.Env):
         _, steer, _ = self.controller.get_commands()
 
         # Apply control
-        act = carla.VehicleControl(throttle=throttle, steer=self.ego.get_control().steer, brake=brake)
+        #act = carla.VehicleControl(throttle=throttle, steer=self.ego.get_control().steer, brake=brake)
+        act = carla.VehicleControl(throttle=throttle, steer=steer, brake=brake)
         self.ego.apply_control(act)
 
         self.world.tick()
