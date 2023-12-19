9e6f100580b84970dbf8a06935f0f28f83858a04
Albertokeroro <alb.moro.2000@gmail.com>
2023-12-19 11:42:11 +0000

    added test cases and configured paths for use with setVar.sh

---
+++ setVar.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SCENARIO_RUNNER_ROOT=/home/carla/NScripts/cases/DAI-AdaptiveCruiseControl/scenario_runner 
+export CARLA_ROOT=/home/carla
+export PYTHONPATH=$PYTHONPATH:/home/carla/PythonAPI/carla/dist/carla-0.9.14-py3.7-linux-x86_64.egg
+export DUR=3600
+export COMET_API_KEY=jUuDMMTUiMF5Oz8x6Tza59Iys
